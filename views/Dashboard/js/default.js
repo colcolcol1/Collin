db5ffa1ce1ca2ab9a28c6793526a47cd701ede0e
colcolcol1 <collin.lambers@hotmail.com>
2018-06-08 13:21:36 +0200

    a

--- views/Dashboard/js/default.js
+++ views/Dashboard/js/default.js
@@ -8,12 +8,14 @@ $(function() {
 
 
   $('#randomInsert').submit(function(){
-    var url = $(this).attr('action');
+    //var url = $(this).attr('action');
     var data = $(this).serialize();
-
-    $.post(url, data, function(o){
+    //
+    // $.post(url, data, function(o){
+    console.log(data);
       alert(1);
     });
+
     return false;
 
   });
